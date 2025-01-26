9d0c984f242db4fa055003464ece002a80703aea
TypeTew <tewsk132@gmail.com>
2025-01-26 18:31:04 +0700

    update

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-
 @Component({
   selector: 'app-user',
   standalone: true,
