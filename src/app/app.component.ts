7dd9c1490f61fb94c182e3997b52e76fd82f10b9
TypeTew <tewsk132@gmail.com>
2025-01-26 18:30:44 +0700

    refactor user component to accept inputs for avatar and name; update app component to display multiple users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent , UserComponent],
-
+  imports: [HeaderComponent, UserComponent],
 })
 export class AppComponent {
-  title = 'angular-app-course';
+  users = DUMMY_USERS;
 }
-
-
-
